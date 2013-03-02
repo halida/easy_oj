74b65ad404ee10eb64d69de82c6e5895b51ff7fb
hadwinzhy <hadwinzhy@gmail.com>
2013-03-02 11:22:12 +0800

    init the tester.py

---
+++ tester.py
@@ -0,0 +1,32 @@
+
+
+
+class TestCase(object):
+    """
+    """
+    language = ''
+    data = ''
+    def __init__(self, ):
+        """
+        """
+        
+        
+
+    def runTest():
+        """
+        1. save the data to a file in tmp folder (when finish run  be deleted)
+        2. run the test
+        3. monitor the memory and time
+        4. if memory || time over, return result
+        5. compare the output, return result
+        """
+        saveToDisk()
+        runScript()
+    
+    
+
+if __name__ == '__main__':
+    TestCase case = TestCase()
+    case.language = "C";
+    case.data = "asdfasdfasdf";
+    case.runTest()
